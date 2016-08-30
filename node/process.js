f4f666c2053d6ffbe18ae0bad957abbc223663b9
GeorgeSalu <george.salu10@gmail.com>
2016-08-29 22:13:43 -0300

    commit inicial, estudo de nodejs

---
+++ node/process.js
@@ -0,0 +1,23 @@
+function prompt(question,callback){
+    'use strict';
+    var stdin = process.stdin;
+    var stdout = process.stdout;
+    
+    stdin.resume();
+    stdout.write(question);
+    
+    stdin.once('data',function(data){
+        callback(data.toString().trim());
+    })
+    
+}
+
+prompt('What is yor name ?',function(data){
+    
+    console.log(data);
+    process.exit();
+})
+
+process.on('exit',function(){
+    console.log('Bye, bye');
+})
